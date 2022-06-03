a6458094d379116130678a66634c57b95a593ede
yuMo <1165435838@qq.com>
2022-06-03 13:14:13 +0800

    使用gitignore

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div> hello World </div>
-        <div> hello World </div>
+        <div> hello World1 </div>
+        <div> hello World2 </div>
+        <div> hello World3 </div>
         <a
           className="App-link"
           href="https://reactjs.org"
